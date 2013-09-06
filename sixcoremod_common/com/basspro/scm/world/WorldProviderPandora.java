afa73024be545a04b5daa19908a0441178566c93
BassPro241 <crashal96@aol.com>
2013-09-06 13:58:47 -0400

    Trying stuff......

---
+++ sixcoremod_common/com/basspro/scm/world/WorldProviderPandora.java
@@ -0,0 +1,12 @@
+package com.basspro.scm.world;
+
+import net.minecraft.world.WorldProvider;
+
+public class WorldProviderPandora extends WorldProvider{
+    
+    public String getDimensionName()
+    {
+        return "Pandora";
+    }
+   
+}
---
+++ sixcoremod_common/com/basspro/scm/world/biome/WorldChunkManagerPandora.java
@@ -0,0 +1,5 @@
+package com.basspro.scm.world.biome;
+
+public class WorldChunkManagerPandora {
+
+}
